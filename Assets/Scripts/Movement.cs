dbcce376245c8d4da1ea57ce209921a524bcfcb2
GinaMackay2014 <113387926+GraceMao2022@users.noreply.github.com>
2025-01-16 14:27:33 -0800

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.localPosition = new Vector3(0, 0, -3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
