2dc3cb2c557582b414b0f26d3f57a82981d31f91
GinaMackay2014 <113387926+GraceMao2022@users.noreply.github.com>
2025-01-16 14:32:38 -0800

    random changes

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.localPosition = new Vector3(0, 0, -3);
-        Debug.Log("hi");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
